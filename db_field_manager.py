c03865d9c2d76523b19656510131f7b23921755d
4332001876 <mzfslhm@163.com>
2023-10-16 16:26:33 +0800

    xml parse

--- db_field_manager.py
+++ db_field_manager.py
@@ -1,15 +1,20 @@
 from crawler import Crawler
 from config import Config
+
 import xml.etree.ElementTree as ET
+import json
+import os
+
 
 class FieldManager:
     def __init__(self) -> None:
-        self._list = []
+        self._dict = {}
         self.crawler = Crawler()
 
     def search(self, keyword):
         """Search for keyword in the list."""
         return
+
 
 class CountryFieldManager(FieldManager):
     def __init__(self) -> None:
@@ -19,9 +24,21 @@ class CountryFieldManager(FieldManager):
         """Get countries list latest updated."""
         url = "https://api.worldbank.org/v2/countries?per_page=5000"  # 目前的国家与地区数目为297个，远小于5000
         content = self.crawler.fetch_data(url)  # 获取内容为XML格式
+        content = content[content.find("<"):]  # 去除XML内容前的非法字符
 
         # 解析XML内容
         root = ET.fromstring(content)
-        print(root)
+        for country in root:
+            country_id = country.attrib["id"]
+            iso2Code = country[0].text
+            country_name = country[1].text
+            # print(country_id, iso2Code, country_name)
 
-        return
+            # 将国家信息存入dict
+            self._dict[country_id] = {
+                "iso2Code": iso2Code, "name": country_name}
+
+        # 将dict存入json文件
+        os.makedirs(Config.TMP_PATH, exist_ok=True)
+        json.dump(self._dict, open(Config.COUNTRIES_LIST_PATH, "w"))
+        return self._dict
--- test.py
+++ test.py
@@ -1,16 +1,16 @@
 from wb_url_manager import WbUrlManager
-from db_field_manager import CountryListManager
+from db_field_manager import CountryFieldManager
 
 
 class Tester:
     def __init__(self) -> None:
         self.wb_url_manager = WbUrlManager()
+        self.country_field_manager = CountryFieldManager()
 
 
     def test_get_countries_list(self):
-        countries_list = self.wb_url_manager.get_countries_list()
+        countries_list = self.country_field_manager.get_countries_list_online()
         print(countries_list)
-        return
 
 
 
