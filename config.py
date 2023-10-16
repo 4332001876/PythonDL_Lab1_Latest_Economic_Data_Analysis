cf31344f565bf2f80a07b6bdaae58b83b034c535
4332001876 <mzfslhm@163.com>
2023-10-16 11:16:36 +0800

    init repo

---
+++ config.py
@@ -0,0 +1,3 @@
+class Config:
+    def __init__(self) -> None:
+        self.USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36 Edg/118.0.2088.46"
